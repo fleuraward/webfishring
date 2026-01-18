bf21564499590ed1c34ab4a8cb4f7f5d0c39601d
fleuraward <128103631+fleuraward@users.noreply.github.com>
2026-01-18 18:51:00 +0000

    added members

--- webfishwbr.js
+++ webfishwbr.js
@@ -19,7 +19,7 @@ const webfishwbr = [
     "host": "sproutvale.nekoweb.org",
     "name": "Beanie",
     "imagefish": "UFO.png",
-    "fish": "Unidentified Fish Object",
+    "fish": "an Unidentified Fish Object",
     "fishline": "...what? the fuck?",
     "nsfw": "No"
   },
@@ -27,8 +27,48 @@ const webfishwbr = [
     "host": "ddnikki.moe",
     "name": "ddnikki",
     "imagefish": "CREATURE.gif",
-    "fish": "CREATURE",
+    "fish": "a CREATURE",
     "fishline": "NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!​NiceCatch!",
+    "nsfw": "No"
+  },
+  {
+    "host": "oceanmew.com",
+    "name": "Ocean",
+    "imagefish": "Coelacanth.png",
+    "fish": "a Coelacanth",
+    "fishline": "Did you see it? I can't!",
+    "nsfw": "no"
+  },
+  {
+    "host": "fruitytales.nekoweb.org",
+    "name": "Viola",
+    "imagefish": "Shrimp.png",
+    "fish": "a Shrimp",
+    "fishline": "One in a krillion! (this isnt a krill btw)",
+    "nsfw": "No"
+  },
+  {
+    "host": "ryqrtz.nekoweb.org",
+    "name": "Rye",
+    "imagefish": "Anomalocaris.png",
+    "fish": "Anomalocaris",
+    "fishline": "Doing it's best :)",
+    "nsfw": "No"
+  },
+  {
+    "host": "scrapsite.nekoweb.org",
+    "name": "BigBanana",
+    "imagefish": "Coelacanth.png",
+    "fish": "a Coelacanth",
+    "fishline": "Did you see it? I can't!",
+    "nsfw": "no"
+  },
+  {
+    "host": "waterlily.nekoweb.org",
+    "name": "Lily",
+    "imagefish": "Helicoprion.png",
+    "fish": "Helicoprion",
+    "fishline": "Nature's pizza cutter!",
     "nsfw": "No"
   }
 ];
